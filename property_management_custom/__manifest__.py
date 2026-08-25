a857a39ed422710c6daced217f722305e568fd44
angelolazo1107 <angelo.lazo1107@gmail.com>
2026-08-25 11:47:21 +0800

    Remove property from depends (was causing build failures) (19.0.1.0.25)

--- property_management_custom/__manifest__.py
+++ property_management_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Property Management Extensions for Odoo Apps',
-    'version': '19.0.1.0.24',
+    'version': '19.0.1.0.25',
     'category': 'Customizations',
     'summary': 'Customizations for Properties, CRM, Sales, Purchase, Maintenance, Helpdesk, Fleet, Approvals & Accounting',
     'description': """
@@ -35,7 +35,6 @@ Integrates with standard Odoo Enterprise Properties App & core modules:
         'website',
         'website_crm',
         'mail',
-        'property',
     ],
     'data': [
         'security/property_management_security.xml',
