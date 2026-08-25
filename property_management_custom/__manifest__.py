538bada1e30094fab0e5d6026795784ce9ce3f02
angelolazo1107 <angelo.lazo1107@gmail.com>
2026-08-25 12:49:50 +0800

    Keep manifest clean for Test: Success build (19.0.1.0.34)

--- property_management_custom/__manifest__.py
+++ property_management_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Property Management Extensions for Odoo Apps',
-    'version': '19.0.1.0.33',
+    'version': '19.0.1.0.34',
     'category': 'Customizations',
     'summary': 'Customizations for Properties, CRM, Sales, Purchase, Maintenance, Helpdesk, Fleet, Approvals & Accounting',
     'description': """
@@ -35,7 +35,6 @@ Integrates with standard Odoo Enterprise Properties App & core modules:
         'website',
         'website_crm',
         'mail',
-        'industry_real_estate',
     ],
     'data': [
         'security/property_management_security.xml',
