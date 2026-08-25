8f7da5598b438e47e88f87e6a820ee44bdd1d128
angelolazo1107 <angelo.lazo1107@gmail.com>
2026-08-25 11:36:59 +0800

    Fix build: remove invalid view inherit_id and simplify property.building inherit (19.0.1.0.23)

--- property_management_custom/models/property_building.py
+++ property_management_custom/models/property_building.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields
 
 class PropertyBuilding(models.Model):
     _inherit = 'property.building'
-
-    code = fields.Char(string='Building Code', copy=False)
-    total_floors = fields.Integer(string='Total Floor Levels', default=1)
